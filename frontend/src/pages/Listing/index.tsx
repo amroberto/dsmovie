68fd84461f4edc1be526c6ac3dd307da208b69da
Alexandre Martins Roberto <amroberto@gmail.com>
2022-05-15 00:50:33 -0300

    useParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,41 +6,40 @@ import Pagination from "../../components/Pagination";
 import { MoviePage } from "types/movie";
 
 function Listing() {
-  
   const [pageNumber, setPageNumber] = useState(0);
 
   const [page, setPage] = useState<MoviePage>({
-      content: [],
-      last: true,
-      totalPages: 0,
-      totalElements: 0,
-      size: 12,
-      number: 0,
-      first: true,
-      numberOfElements: 0,
-      empty: true
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
   });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
-    .then(response  => {
-      const data = response.data as MoviePage;
-      setPage(data);
-    });
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
   }, [pageNumber]);
-  
+
   return (
     <>
       <Pagination />
 
       <div className="container">
         <div className="row">
-            {page.content.map(movie => (
-              <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                  <MovieCard movie={movie} />
-              </div>
-            )
-            )}          
+          {page.content.map(movie => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          ))}
         </div>
       </div>
     </>
